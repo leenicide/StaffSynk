5de6ae9dd1f91b9cb2bd7fc2f80924fca6d4a5b0
Aawaiz Ali <aawaizali8@gmail.com>
2025-08-01 12:50:06 +0500

    more dependencies

--- test-ios/staffsynk/macos/Flutter/GeneratedPluginRegistrant.swift
+++ test-ios/staffsynk/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,6 +11,7 @@ import firebase_core
 import firebase_messaging
 import flutter_secure_storage_macos
 import geolocator_apple
+import local_auth_darwin
 import package_info_plus
 import path_provider_foundation
 import shared_preferences_foundation
@@ -23,6 +24,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  LocalAuthPlugin.register(with: registry.registrar(forPlugin: "LocalAuthPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
--- test-ios/staffsynk/macos/Runner/AppDelegate.swift
+++ test-ios/staffsynk/macos/Runner/AppDelegate.swift
@@ -1,6 +1,7 @@
 import Cocoa
 import FlutterMacOS
 
+
 @main
 class AppDelegate: FlutterAppDelegate {
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
